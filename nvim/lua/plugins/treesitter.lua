a9b7829fbba16c74cad1e14b3b034c4b28fbaa87
Ahmed Essam <aessamby@gmail.com>
2026-05-07 03:01:48 +0300

    disable .md file parsing with treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,8 +6,20 @@ return {
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "lua", "vim", "vimdoc", "bash", "json", "yaml", "markdown", "markdown_inline" },
 			auto_install = true,
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+				-- Disable highlighting for markdown to avoid the bug
+				disable = { "markdown" },
+			},
 			indent = { enable = true },
+		})
+
+		-- Disable treesitter for markdown and use built-in syntax
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "markdown",
+			callback = function()
+				vim.treesitter.stop()
+			end,
 		})
 	end,
 }
