5d2cf8134bc7a55f339678cac1a00af16f0c2f51
Ahmed Essam <aessamby@gmail.com>
2026-05-28 05:24:40 +0300

    update conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,14 +7,7 @@ return {
       go = { "gofmt", "goimports" },
       templ = { "templ" },
     },
-    -- Define the templ formatter (runs `templ fmt` via stdin)
-    formatters = {
-      templ = {
-        command = "templ",
-        args = { "fmt", "-" },
-        stdin = true,
-      },
-    },
+
     format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
   },
 }
